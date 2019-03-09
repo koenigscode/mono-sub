9ddc83673ed17e6871a5daf099202cdde5dd814f
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-09 18:54:32 +0100

    generate mappings when textarea changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="h-full h-screen">
+    <div class="container-grid h-full">
+      <textarea v-model="srcText" id="area-src" class="textarea h-full"></textarea>
+      <textarea id="area-tgt" class="textarea h-full" disabled></textarea>
+      <div class="mapping-container">
+        <char-mapping v-for="(c, i) in mapping" :key="i" :label="c"></char-mapping>
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CharMapping from "./components/CharMapping.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    CharMapping
+  },
+  data() {
+    return {
+      srcText: "",
+      mapping: new Set()
+    };
+  },
+  watch: {
+    srcText(str) {
+      this.mapping = new Set(str.split(""));
+      console.log(this.mapping.size);
+    }
   }
-}
+};
 </script>
-<style lang="stylus">
-#app
+
+<style lang="stylus" scoped>
+#app 
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  text-align: center
+
+.container-grid 
+  display grid
+  grid-template-columns 1fr 1fr
+  min-height 30rem
+
+.mapping-container
+  display grid
+  git grid-template-columns 1fr 1fr 1fr
+  grid-auto-rows minmax(min-content, max-content)
+
+  @media (min-width 768px)
+    grid-template-columns 1fr 1fr 1fr 1fr 1fr
+
+  @media (min-width 1200px)
+    grid-template-columns 1fr 1fr 1fr 1fr 1fr 1fr 1fr
+  
+
+textarea:disabled
+  background-color white
+
 </style>
---
+++ src/components/CharMapping.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="inline m-2">
+    <div class="flex justify-end">
+      <span class="mx-2">{{label}}:</span>
+      <input v-model="input" type="text">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "char-mapping",
+  props: ["label", "value"],
+  data() {
+    return {
+      input: this.value
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+input
+  width 3rem
+  background gray
+</style>
