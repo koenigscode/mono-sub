699b9018d31a0b79ef6c38717fdc72d0c0d41026
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-15 18:12:09 +0100

    basic plotting functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,112 @@
 import React, { Component } from "react";
+import "./components/FrequencyChart.js";
 import "./App.scss";
+import FrequencyChart from "./components/FrequencyChart";
+import gerFreq from "./data/frequency-german.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      srcText: "",
+      tgtText: "",
+      mapping: {}
+    };
+  }
+
+  get srcFreq() {
+    let freq = {};
+    for (let c of this.chars) {
+      let count = this.state.srcText.toUpperCase().split(c).length - 1;
+      freq[c] = (count / this.state.srcText.length) * 100;
+    }
+    const ordered = {};
+    Object.values(freq)
+      .sort()
+      .forEach(function(key) {
+        ordered[key] = freq[key];
+      });
+    return freq;
+  }
+
+  get chars() {
+    if (this.state.srcText.trim().length === 0) return [];
+    let s = new Set();
+    for (let c of this.state.srcText.split("")) {
+      if (c.match(/^[0-9a-zA-Z]+$/))
+        s.add(
+          c in this.state.mapping
+            ? this.state.mapping[c.toUpperCase()]
+            : c.toUpperCase()
+        );
+    }
+    return s;
+  }
+
+  get maxTick() {
+    if (this.srcFreq !== undefined) {
+      let maxVal = Math.max(
+        ...Object.values(this.srcFreq),
+        ...Object.values(gerFreq)
+      );
+      return 2 * Math.round(Math.ceil(maxVal) / 2);
+    }
+  }
+
+  mappingChange(key, val) {
+    this.state.mapping[key] = val;
+    this.updateOutput();
+  }
+
+  updateOutput() {
+    let s = "";
+    for (let c of this.state.srcText) {
+      if (c.toUpperCase() in this.state.mapping) {
+        s += this.state.mapping[c.toUpperCase()];
+      } else {
+        s += c;
+      }
+    }
+    this.setState({ tgtText: s });
+  }
+
+  loadExample() {
+    this.setState({ srcText: "exampleText" });
+  }
+
+  onChange = (prop, e) => this.setState({ [prop]: e.target.value });
+
   render() {
     return (
       <div className="app h-full w-full">
         <div className="container-grid min-h-screen h-full">
-          <textarea />
-          <textarea />
+          <textarea
+            ref="area-src"
+            value={this.state.srcText}
+            placeholder="Erwarte Eingabe.."
+            className="textarea h-full p-4"
+            onChange={e => this.onChange("srcText", e)}
+          />
+          <textarea
+            ref="area-tgt"
+            value={this.state.tgtText}
+            className="textarea h-full p-4"
+            placeholder="Hier erscheint der Output.."
+            onChange={e => this.onChange("tgtText", e)}
+            maxtick={this.state.maxTick}
+            disabled
+          />
           <div className="mapping-container" />
+          <FrequencyChart
+            title="Durchschnittliche Buchstabenhäufigkeit in deutschen Texten"
+            chartdata={gerFreq}
+            maxtick={this.maxTick}
+          />
+          <FrequencyChart
+            title="Zeichenhäufigkeit im eingegebenen Text"
+            chartdata={this.srcFreq}
+            maxtick={this.maxTick}
+          />
         </div>
       </div>
     );
---
+++ src/components/FrequencyChart.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import "./FrequencyChart.scss";
+import colors from "../scss/_variables.scss";
+
+class FrequencyChart extends Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+    this.canvasWrapper = React.createRef();
+    this.state = { chart: null };
+  }
+
+  get sortedData() {
+    let keys = Object.keys(this.props.chartdata);
+    let values = Object.values(this.props.chartdata);
+    let data = keys.map(function(e, i) {
+      return [e, values[i]];
+    });
+    data.sort((a, b) => {
+      return b[1] - a[1];
+    });
+    return data;
+  }
+
+  get labels() {
+    let arr = [];
+    for (let i of this.sortedData) {
+      arr.push(i[0]);
+    }
+    return arr;
+  }
+
+  get values() {
+    let arr = [];
+    for (let i of this.sortedData) {
+      arr.push(i[1]);
+    }
+    return arr;
+  }
+
+  createChart(ctx) {
+    this.setState({
+      // eslint-disable-next-line no-undef
+      chart: new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: this.props.labels,
+          datasets: [
+            {
+              data: this.props.values,
+              backgroundColor: colors.accent + "40",
+              hoverBackgroundColor: colors.accent + "60",
+              borderColor: colors.accent,
+              borderWidth: 2
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: this.props.title
+          },
+          responsive: true,
+          tooltips: {
+            displayColors: false,
+            callbacks: {
+              label: item =>
+                `Häufigkeit: ${Math.round(item.yLabel * 100) / 100}%`
+            }
+          },
+          scales: {
+            yAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Häufigkeit in Prozent"
+                },
+                ticks: {
+                  beginAtZero: true,
+                  max: this.props.maxtick
+                }
+              }
+            ]
+          }
+        }
+      })
+    });
+  }
+
+  updateChart() {
+    if (this.state.chart == null) {
+      this.createChart(this.canvas.current.getContext("2d"));
+    } else if (Object.keys(this.props.chartdata).length == 0) {
+      this.state.chart.destroy();
+      this.state.chart = null;
+    } else {
+      this.state.chart.data.labels = this.labels;
+      this.state.chart.data.datasets[0].data = this.values;
+      this.state.chart.options.scales.yAxes[0].ticks.max = this.props.maxtick;
+      this.state.chart.update();
+    }
+  }
+
+  componentDidUpdate() {
+    this.updateChart();
+  }
+
+  componentDidMount() {
+    if (Object.keys(this.props.chartdata).length > 0) {
+      this.createChart(this.canvas.current.getContext("2d"));
+    }
+  }
+
+  render() {
+    return (
+      <div ref={this.canvasWrapper} className="h-full overflow-hidden">
+        <canvas ref={this.canvas} />
+      </div>
+    );
+  }
+}
+
+export default FrequencyChart;
