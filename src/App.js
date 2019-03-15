6b1b2a854bb708a65348a76318893d9116e48fde
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-03-15 16:27:00 +0100

    add global variables

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app h-full w-full">
+        <div className="container-grid min-h-screen h-full">
+          <textarea />
+          <textarea />
+          <div className="mapping-container" />
+        </div>
       </div>
     );
   }
